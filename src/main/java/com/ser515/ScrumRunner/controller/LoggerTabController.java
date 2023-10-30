ccb906c07fe2972bdbab01655cfc2085356ead03
aramani9 <aramani9@asu.edu>
2023-10-30 15:12:17 -0700

    Task TG-40 for US-28

--- src/main/java/com/ser515/ScrumRunner/controller/LoggerTabController.java
+++ src/main/java/com/ser515/ScrumRunner/controller/LoggerTabController.java
@@ -2,6 +2,7 @@ package com.ser515.ScrumRunner.controller;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.Node;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.Tab;
@@ -12,26 +13,19 @@ import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
 import org.springframework.stereotype.Component;
 
+import java.io.File;
 import java.util.Objects;
+import java.util.Random;
 
 @Component
 public class LoggerTabController {
-
-    @FXML
-    private ImageView imageView2;
-
-    // Initialize the animated GIF
-//    public void initialize() {
-////        String gifPath = "@../../assets/dice-roll.gif"; // Replace with the actual path or URL to your animated GIF
-//        String gifPath = "https://media.tenor.com/YfLnhkuHzzAAAAAC/dice-roll.gif";
-//        Image gifImage = new Image(Objects.requireNonNull(getClass().getResource(gifPath)).toExternalForm());
-//        imageView2.setImage(gifImage);
-//    }
 
 
     public Tab tabLogger;
 
     public ImageView imageView1;
+    public ImageView diceImage;
+    public Button rollButton;
 
     @FXML
     private Button btnSpring;
@@ -46,5 +40,29 @@ public class LoggerTabController {
         btnSpring.setDisable(true);
     }
 
+    @FXML
+    public void roll(ActionEvent actionEvent) {
+//        System.out.println("Button Clicked");
+        rollButton.setDisable(true);
 
+        Thread thread = new Thread(() -> {
+            System.out.println("Thread Running");
+            try {
+                for (int i = 0; i < 15; i++) {
+//                    diceImage = new ImageView();
+                    Random random = new Random();
+//                    System.out.println(random.nextInt(6)+1);
+                    File file = new File("@../../assets/dice-" + (random.nextInt(6)+1)+".png");
+                    diceImage.setImage(new Image(String.valueOf(file)));
+//                    System.out.println(file);
+                    Thread.sleep(50);
+                }
+                rollButton.setDisable(false);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
+        thread.start();
+
+    }
 }
