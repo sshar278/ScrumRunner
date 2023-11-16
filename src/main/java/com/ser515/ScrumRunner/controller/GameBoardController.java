d5d5ac2013a528f052368846f6dbd6b9fe958f30
Karthik Vaida <jvaida@asu.edu>
2023-11-16 15:33:19 -0700

    Task TG-90 for US-34

--- src/main/java/com/ser515/ScrumRunner/controller/GameBoardController.java
+++ src/main/java/com/ser515/ScrumRunner/controller/GameBoardController.java
@@ -8,6 +8,8 @@ import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
 import javafx.scene.image.Image;
 import javafx.scene.control.Tab;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.Pane;
 import javafx.scene.shape.Circle;
 import org.springframework.stereotype.Component;
 import java.io.File;
@@ -53,4 +55,11 @@ public class GameBoardController {
         thread.start();
 
     }
+
+    @FXML
+    private void handlePaneClick(MouseEvent event) {
+        Pane clickedPane = (Pane) event.getSource();
+        System.out.println("Clicked Pane ID: " + clickedPane.getId());
+    }
+
 }
